78307f87cf593b3bc3e62dbef4701e73288f27df
ambueno <amandabuenocampos@gmail.com>
2022-10-01 23:22:28 -0300

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,43 @@
+--Seleciona o último nome de todos os empregados
+
+--Seleciona o último nome, primeiro nome, e cargo de todos os empregados
+
+--Seleciona o primeiro e último nome de todos os empregados, ordenados pelo último nome
+
+--Seleciona todos os empregados que são representantes de venda (Sales rep)
+
+--Seleciona todos os empregados que não são representantes de venda e estão lotados no escritório de código 1
+
+--Seleciona o nome e telefone dos clientes que possuem limite de crédito entre 50.000 e 100.000
+
+--Seleciona todos os funcionários que são VP na empresa
+
+--Seleciona o nome e cargo dos funcionários que não possuem chefe na empresa
+
+--Seleciona os clientes que sejam da França ou Estados Unidos
+
+--Seleciona os nomes dos produtos junto de seu texto de descrição
+
+--Seleciona os empregados que estejam lotados em escritórios dos Estados Unidos
+
+--Seleciona os pedidos realizados por clientes da França
+
+--Seleciona o número e data do pedido, junto da quantidade e nome dos produtos pedidos
+
+--Seleciona os clientes que ainda não realizaram nenhum pedido
+
+--Seleciona o nome dos empregados junto do nome de seus superiores
+
+--Seleciona a quantidade de pedidos realizados por status
+
+--Seleciona o valor total de cada pedido
+
+--Seleciona a quantidade de pedidos realizados por ano
+
+--Seleciona a quantidade de pedidos realizados e entregues por ano
+
+--Seleciona a quantidade de pedidos realizados cujo valor total seja superior a 100.000
+
+--Seleciona os pedidos que tiveram mais de 200 itens encomendados, ordenados pela quantidade de itens
+
+--Seleciona os pedidos que tiveram mais de 500 itens encomendados e que custaram mais de 60.000, ordenados pela quantidade
